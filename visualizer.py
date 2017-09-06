5927662b343ecb2e693c816ca11ca8cbcfe13e23
Ian Howell <ian.howell0@gmail.com>
2017-09-05 22:42:22 -0500

    Move usage into its own function, and return length with get_shapes

--- visualizer.py
+++ visualizer.py
@@ -8,7 +8,7 @@ def main():
 
 def get_shapes():
     try:
-        shapefilename, placementfilename = sys.argv[1], sys.argv[2]
+        shapefilename, placementfilename, length = sys.argv[1:]
         shapes = []
         with open(shapefilename) as sf, open(placementfilename) as pf:
             width, _ = (int(x) for x in sf.readline().strip().split(' '))
@@ -19,14 +19,20 @@ def get_shapes():
                 shape['loc'] = tuple(intermediate[:2])
                 shape['rot'] = intermediate[2]
                 shapes.append(shape)
-        return shapes
+        return width, int(length), shapes
     except IndexError:
-        usage = "Usage: python {} <shapefile> <placementfile>"
-        print(usage.format(sys.argv[0]), file=sys.stderr)
+        usage()
         exit(1)
     except FileNotFoundError as e:
         print(e, file=sys.stderr)
         exit(1)
+
+
+def usage():
+    usage_str = """
+                Usage: python {} <shapefile> <placementfile> <required_length>
+                """
+    print(usage_str.format(sys.argv[0]), file=sys.stderr)
 
 
 if __name__ == "__main__":
