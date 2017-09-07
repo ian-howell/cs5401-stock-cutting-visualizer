f594406ab9e7287db7a65289b33f0175d4d7c11b
Ian Howell <ian.howell0@gmail.com>
2017-09-07 17:36:12 -0500

    Add argparsing and gridlines

--- visualizer.py
+++ visualizer.py
@@ -5,13 +5,15 @@
 # Date Created:   09-05-2017
 # Python Version: 3.4
 
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
 
-def main():
-    width, length, shapes = get_shapes()
+def main(args):
+    width, shapes = get_shapes(args['shapefile'], args['placementfile'])
+    length = args['length']
 
     arr = np.array([0] * length * width).reshape((width, length))
 
@@ -24,6 +26,10 @@ def main():
     axes.imshow(arr)
     axes.invert_yaxis()
 
+    if args['grid']:
+        axes.grid(which='major', linewidth=1, alpha=0.5)
+        axes.grid(which='minor', linewidth=1, alpha=0.25)
+
     y_ticks = [y for y in range(0, width, 5)]
     x_ticks = [x for x in range(0, length, 5)]
     plt.yticks(y_ticks)
@@ -33,26 +39,18 @@ def main():
     plt.show()
 
 
-def get_shapes():
-    try:
-        shapefilename, placementfilename, length = sys.argv[1:]
-        shapes = []
-        with open(shapefilename) as sf, open(placementfilename) as pf:
-            width, _ = (int(x) for x in sf.readline().strip().split(' '))
-            for cut, place_rot in zip(sf, pf):
-                shape = {}
-                shape['cut'] = tuple(cut.strip().split(','))
-                intermediate = [int(x) for x in place_rot.strip().split(',')]
-                shape['loc'] = tuple(intermediate[:2])
-                shape['rot'] = intermediate[2]
-                shapes.append(shape)
-        return width, int(length), shapes
-    except FileNotFoundError as e:
-        print(e, file=sys.stderr)
-        exit(1)
-    except:
-        usage()
-        exit(1)
+def get_shapes(shapefilename, placementfilename):
+    shapes = []
+    with open(shapefilename) as sf, open(placementfilename) as pf:
+        width, _ = (int(x) for x in sf.readline().strip().split(' '))
+        for cut, place_rot in zip(sf, pf):
+            shape = {}
+            shape['cut'] = tuple(cut.strip().split(','))
+            intermediate = [int(x) for x in place_rot.strip().split(',')]
+            shape['loc'] = tuple(intermediate[:2])
+            shape['rot'] = intermediate[2]
+            shapes.append(shape)
+    return width, shapes
 
 
 def cut_shape(arr, shape, shape_id):
@@ -77,11 +75,18 @@ def cut_shape(arr, shape, shape_id):
     return arr
 
 
-def usage():
-    usage_str = "Usage: python {}"
-    usage_str += " <shapefile> <placementfile> <required_length>"
-    print(usage_str.format(sys.argv[0]), file=sys.stderr)
-
-
 if __name__ == "__main__":
-    main()
+    # Get arguments
+    parser = argparse.ArgumentParser(prog=sys.argv[0])
+    parser.add_argument('-g', '--grid', action='store_true',
+                        help='Turn on gridlines')
+    parser.add_argument('-s', '--shapefile', action='store', required=True,
+                        help='File containing the list of shapes')
+    parser.add_argument('-p', '--placementfile', action='store', required=True,
+                        help='File containing the placement of shapes')
+    parser.add_argument('-l', '--length', action='store',
+                        type=int, required=True,
+                        help='Maximum required length for solution')
+    args = parser.parse_args()
+
+    main(vars(args))
